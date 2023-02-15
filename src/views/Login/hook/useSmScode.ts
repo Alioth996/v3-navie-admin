e2dbcf6b3f7b70f3a588239c0e1a6301d87fb5b3
alex <2844520415@qq.com>
2023-02-15 11:34:46 +0800

    chore:进一步抽离逻辑为hooks

--- src/views/Login/hook/useSmScode.ts
+++ src/views/Login/hook/useSmScode.ts
@@ -1,46 +1,68 @@
-import {ref} from 'vue'
+import { computed, ref } from "vue";
+import { useTimer } from "./common/useTimer";
+
+function useBoolean(value: boolean) {
+  let bool = ref(value);
+
+  function setFalse() {
+    bool.value = false;
+  }
+
+  function setTrue() {
+    bool.value = true;
+  }
+
+  function toggleBool() {
+    bool.value = !bool.value;
+  }
+
+  return {
+    bool,
+    setFalse,
+    setTrue,
+    toggleBool,
+  };
+}
 
 export default function useCount(second: number = 5) {
   if (second <= 0 && second % 1 !== 0) {
-    throw new Error('倒计时的时间应该为一个正整数！')
+    throw new Error("倒计时的时间应该为一个正整数！");
   }
 
-  let count = ref(second)
-  const label = ref('获取验证码')
-  const disabled = ref(false)
+  let count = ref(0);
 
-  let timerId: any
+  const { bool: disabled, setTrue, setFalse } = useBoolean(false);
+
+  let initLabel = "获取验证码";
+
+  const label = computed(() => {
+    let text = initLabel;
+    if (Boolean(count.value)) {
+      text = `倒计时${count.value} 秒`;
+    }
+    return text;
+  });
+
+  const { clearTimer, setTimer } = useTimer();
 
   function startCount() {
-    clear()
-    disabled.value = true
-
-    timerId = setInterval(() => {
-      label.value = `倒计时 ${count.value} 秒`
-      count.value--
-
-      if (count.value < 0) {
-        label.value = `获取验证码`
-        disabled.value = false
-        count.value = second
-        clear()
-
+    clearTimer();
+    // 每次启动重新赋值
+    count.value = second;
+    setTrue();
+    setTimer(() => {
+      count.value -= 1;
+      if (count.value <= 0) {
+        setFalse();
+        clearTimer();
       }
-
-    }, 1000)
+    }, 1000);
   }
-
-  function clear() {
-    if (timerId) {
-      clearInterval(timerId)
-    }
-  }
-
 
   return {
     count,
     label,
     disabled,
-    startCount
-  }
+    startCount,
+  };
 }
