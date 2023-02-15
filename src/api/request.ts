25d39d110f2b3b9bb2bcfcff45ed2a0a42a4a8f4
alex <2844520415@qq.com>
2023-02-15 12:42:55 +0800

    feat: 新增验证码验证,用户密码验证

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,23 +1,14 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 const instance: AxiosInstance = axios.create({
-    baseURL: '/',
-    timeout: 3000
-})
+  baseURL: "/",
+  timeout: 3000,
+});
 
-
-instance.interceptors.response.use(async res => {
-    const {code, message} = res.data
-    if (code >= 400) {
-        // @ts-ignore
-        return Promise.reject(message)
-    }
-    return res.data
-})
+instance.interceptors.response.use(async (res) => {
+  return res.data;
+});
 
 export const request = async <T>(config: AxiosRequestConfig): Promise<T> => {
-    return await instance.request(config)
-
-}
-
-
+  return await instance.request(config);
+};
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,28 +1,50 @@
-import { token, userList } from './user'
+import { token, userList } from "./user";
 
-import Mock from 'mockjs'
-import { loginResOption } from '../@types/login'
+import Mock from "mockjs";
+import { loginModelType, loginResOption } from "../@types/login";
+
+let captcha: string;
 
 const useMock = () => {
-  Mock.mock('/mock/login', 'post', (option: any) => {
-    return {
-      data: token,
-      code: 200,
-      message: 'success'
-    } as resOption<loginResOption>
-  })
+  Mock.mock("/mock/login", "post", (option: any) => {
+    const { username, password, verify_code } = JSON.parse(
+      option.body
+    ) as loginModelType;
 
-  Mock.mock('/mock/verify_code', 'post', (option: any) => {
-    const { phone } = JSON.parse(option.body)
-    const captcha = Math.random().toFixed(6).slice(-6)
+    if (verify_code == captcha) {
+      if (username !== "admin" || !password) {
+        return {
+          data: null,
+          code: 400,
+          message: "用户名或密码错误",
+        } as resOption<null>;
+      } else {
+        return {
+          data: null,
+          token,
+          code: 200,
+          message: "success",
+        } as resOption<loginResOption>;
+      }
+    } else {
+      return {
+        data: null,
+        code: 400,
+        message: "验证码错误",
+      } as resOption<null>;
+    }
+  });
+
+  Mock.mock("/mock/verify_code", "post", (option: any) => {
+    captcha = Math.random().toFixed(6).slice(-6);
     return {
       data: null,
       captcha,
       code: 200,
-      message: '验证码获取成功'
-    } as resOption<null>
-  })
-  Mock.mock('/mock/user', userList)
-}
+      message: "验证码获取成功",
+    } as resOption<null>;
+  });
+  Mock.mock("/mock/user", userList);
+};
 
-export default useMock
+export default useMock;
